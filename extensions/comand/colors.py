36eac9dd3dfc0bfb8af12a8dc579679a0e38c16e
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2023-04-07 16:47:36 -0300

    feat: role colors
    Create role with a hex color

---
+++ extensions/comand/colors.py
@@ -0,0 +1,21 @@
+from discord import app_commands
+from discord.ext import commands
+
+
+class ColorsCommand(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.hybrid_command()
+    async def colors(self, ctx: commands.Context, name: str, hex: str):
+        '''Create role with hex color'''
+
+        role = await ctx.guild.create_role(name=name, colour=int(hex.replace('#', ''), 16))
+        
+        await ctx.guild.edit_role_positions({role: len(ctx.guild.roles)-7})
+
+        await ctx.send(role.mention)
+
+async def setup(bot):
+    await bot.add_cog(ColorsCommand(bot))
