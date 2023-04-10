13f7d5a6e533f6dc8c77641efc953ee8795c85fc
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2023-04-10 01:29:28 -0300

    refactor: changed from prefix to slash
    refactor: changed embed_color comand to slash command and added a time limit to interact with the buttons

--- extensions/comand/colors.py
+++ extensions/comand/colors.py
@@ -2,6 +2,7 @@ import discord
 
 from discord import app_commands
 from discord.ext import commands
+from time import time
 
 from extensions import config
 from extensions.views.colors_view import ColorsView
@@ -11,11 +12,6 @@ class ColorsCommand(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.colors_view = ColorsView(timeout=None)
-
-    async def cog_load(self):
-        self.bot.add_view(self.colors_view)
-
 
     @commands.has_permissions(kick_members=True)
     @commands.command()
@@ -32,21 +28,19 @@ class ColorsCommand(commands.Cog):
 
         await ctx.send(role.mention)
 
-    @commands.has_permissions(kick_members=True)
-    @commands.command()
-    async def embed_colors(self, ctx: commands.Context):
+    @app_commands.command(name='colors')
+    @app_commands.checks.has_role(config.tof_member)
+    async def embed_colors(self, interaction: discord.Interaction):
         '''Create a embed colors'''
 
-        em=discord.Embed(color=config.cinza, title='Auto Color', description='')
+        em=discord.Embed(color=config.cinza, title='Auto Color', description=f'Countdown: <t:{int(time())+180}:R>')
         em.set_footer(text='Choose your color')
 
-        colors = config.colors_list
-
-        for color_id in colors:
-            color = ctx.guild.get_role(color_id)
+        for color_id in config.colors_list:
+            color = interaction.guild.get_role(color_id)
             em.description+=f'{color.mention}\n'
 
-        await ctx.send(embed=em, view=ColorsView())
+        await interaction.response.send_message(embed=em, ephemeral=True, view=ColorsView(timeout=180))
 
 
 async def setup(bot):
