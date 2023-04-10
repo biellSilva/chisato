fd9e2895b0a6b15c6e78c48443a2f41a8c545b95
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2023-04-10 00:49:45 -0300

    feat: choose colors
    embed to choose colors

--- extensions/comand/colors.py
+++ extensions/comand/colors.py
@@ -1,5 +1,9 @@
+import discord
+
 from discord import app_commands
 from discord.ext import commands
+
+from extensions import config
 
 
 class ColorsCommand(commands.Cog):
@@ -7,7 +11,8 @@ class ColorsCommand(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.hybrid_command()
+    @commands.has_permissions(kick_members=True)
+    @commands.command()
     async def colors(self, ctx: commands.Context, name: str, hex: str):
         '''Create role with hex color'''
 
@@ -20,6 +25,23 @@ class ColorsCommand(commands.Cog):
         await ctx.guild.edit_role_positions({role: len(ctx.guild.roles)-7})
 
         await ctx.send(role.mention)
+
+    @commands.has_permissions(kick_members=True)
+    @commands.command()
+    async def embed_colors(self, ctx: commands.Context):
+        '''Create a embed colors'''
+
+        em=discord.Embed(color=config.cinza, title='Auto Color', description='')
+        em.set_footer(text='Choose your color')
+
+        colors = config.colors_list
+
+        for color_id in colors:
+            color = ctx.guild.get_role(color_id)
+            em.description+=f'{color.mention} - {color.color}\n'
+
+        await ctx.send(embed=em)
+
 
 async def setup(bot):
     await bot.add_cog(ColorsCommand(bot))
