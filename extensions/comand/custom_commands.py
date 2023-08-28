5f2311789fb42e0866355357f56da215fcfe7559
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-08-28 13:35:27 -0300

    a

--- extensions/comand/custom_commands.py
+++ extensions/comand/custom_commands.py
@@ -50,6 +50,8 @@ class CustomCommandsCog(commands.GroupCog, group_name='commands', group_descript
                 }
             })
 
+            print(self.data)
+
             with open('./extensions/database/CustomCommands.json', 'w') as f:
                 data = json.dumps(obj=self.data, indent=2)
                 f.write(data)
