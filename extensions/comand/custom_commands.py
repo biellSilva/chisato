e8d6328e93a22eb477fdb2a8e12c965d8e1b554c
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-09-15 12:52:27 -0300

    edit e delete comandos

--- extensions/comand/custom_commands.py
+++ extensions/comand/custom_commands.py
@@ -1,5 +1,6 @@
 import discord
 import json
+import re
 
 from discord import app_commands
 from discord.ext import commands
@@ -30,11 +31,15 @@ class CustomCommandsCog(commands.GroupCog, group_name='commands', group_descript
 
 
     @app_commands.command(name='add')
-    async def add_command(self, interaction: discord.Interaction, name: str, content: Optional[str], file: Optional[discord.Attachment]):
+    async def add_commando(self, interaction: discord.Interaction, name: str, content: Optional[str], file: Optional[discord.Attachment]):
         await interaction.response.defer(ephemeral=True)
 
         if name.lower() in self.data:
             await interaction.edit_original_response(content=f'{name.lower()} já existe como comando')
+            return
+        
+        if '<@' in name or '<@' in content:
+            await interaction.edit_original_response(content=f'sem menção no comando!!')
 
         else:
             if file:
@@ -57,6 +62,70 @@ class CustomCommandsCog(commands.GroupCog, group_name='commands', group_descript
                 f.write(data)
         
             await interaction.edit_original_response(content=f'c!{name.lower()}')
+    
+    @app_commands.command(name='edit')
+    async def edit_commando(self, interaction: discord.Interaction, name: str, content: Optional[str], file: Optional[discord.Attachment]):
+        await interaction.response.defer(ephemeral=True)
+
+        if name.lower() not in self.data:
+            await interaction.edit_original_response(content=f'{name.lower()} não existe')
+            return
+
+        comando: dict[str, str | int | None] = self.data.get(name.lower())
+
+        if interaction.user.id != comando.get('author_id'):
+            await interaction.edit_original_response(content=f'{name.lower()} não lhe pertence')
+            return
+        
+        if '<@' in name or '<@' in content:
+            await interaction.edit_original_response(content=f'sem menção no comando!!')
+
+        else:
+            if file:
+                with open(f'./extensions/assets/custom_commands/{interaction.user.id}_{name.lower()}.{file.content_type.split("/")[1]}', 'wb') as f:
+                    f.write(await file.read())
+
+            self.data.update({
+                name.lower() : {
+                    'name': name,
+                    'content': content,
+                    'file': file if file else comando.get('file'),
+                    'file_format': None if not file else comando.get('file_format'),
+                    'author': interaction.user.name,
+                    'author_id': str(interaction.user.id)
+                }
+            })
+
+            with open('./extensions/database/CustomCommands.json', 'w') as f:
+                data = json.dumps(obj=self.data, indent=2)
+                f.write(data)
+        
+            await interaction.edit_original_response(content=f'c!{name.lower()}')
+    
+
+    @app_commands.command(name='delete')
+    async def delete_commando(self, interaction: discord.Interaction, name: str):
+        await interaction.response.defer(ephemeral=True)
+
+        if name.lower() not in self.data:
+            await interaction.edit_original_response(content=f'{name.lower()} não existe')
+            return
+
+        comando: dict[str, str | int | None] = self.data.get(name.lower())
+
+        if interaction.user.id != comando.get('author_id') and not interaction.user.guild_permissions.administrator:
+            await interaction.edit_original_response(content=f'{name.lower()} não lhe pertence')
+            return
+        
+        else:
+
+            del self.data[name.lower()]
+
+            with open('./extensions/database/CustomCommands.json', 'w') as f:
+                data = json.dumps(obj=self.data, indent=2)
+                f.write(data)
+        
+            await interaction.edit_original_response(content=f'c!{name.lower()} deletado')
 
     
     @commands.Cog.listener(name='on_message')
