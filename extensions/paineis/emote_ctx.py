a84ba08a68e92bb4e40d78dde2292b82d9d0140a
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-09-25 21:37:42 -0300

    emote limite response

--- extensions/paineis/emote_ctx.py
+++ extensions/paineis/emote_ctx.py
@@ -12,11 +12,7 @@ class RandomContextMenus(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
-        self.add_emote = app_commands.ContextMenu(
-            name='Add Emote',
-            callback=self.add_emote_menu
-        )
+        self.add_emote = app_commands.ContextMenu(name='Add Emote', callback=self.add_emote_menu)
 
     async def cog_load(self):
         self.bot.tree.add_command(self.add_emote)
@@ -35,9 +31,12 @@ class RandomContextMenus(commands.Cog):
         names: list = message.content.split()
 
         for name, emoji in zip(names, message.attachments):
-            added_emote = await interaction.guild.create_custom_emoji(name=name, image=await emoji.read())
-            await interaction.edit_original_response(content=added_emote)
-        
-    
+            try:
+                added_emote = await interaction.guild.create_custom_emoji(name=name, image=await emoji.read())
+                await interaction.edit_original_response(content=added_emote)
+            except discord.HTTPException as err:
+                if err.code == 30008:
+                    await interaction.edit_original_response(content=err.text)
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(RandomContextMenus(bot))
